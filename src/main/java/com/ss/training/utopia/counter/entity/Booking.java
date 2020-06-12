94e099777c8cc1ce20a4161d47692b95e74a7bd6
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-12 15:00:28 -0600

    Start booking class

---
+++ src/main/java/com/ss/training/utopia/counter/entity/Booking.java
@@ -0,0 +1,15 @@
+package com.ss.training.utopia.counter.entity;
+
+/**
+ * @author Justin O'Brien
+ */
+public class Booking {
+
+	/**
+	 * 
+	 */
+	public Booking() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
