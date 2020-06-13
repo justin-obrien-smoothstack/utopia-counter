f54e09fd3a9d42d2d6fe9e38665405fbd2c9bff4
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-13 14:36:25 -0600

    Start booking service class

---
+++ src/main/java/com/ss/training/utopia/counter/service/BookingService.java
@@ -0,0 +1,11 @@
+package com.ss.training.utopia.counter.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Justin O'Brien
+ */
+@Component
+public class BookingService {
+
+}
