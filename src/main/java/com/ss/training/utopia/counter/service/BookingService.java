97f64e1f01e256d7caeea50ac8e107fff31ca65f
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-13 16:32:24 -0600

    Write create user method in booking service

--- src/main/java/com/ss/training/utopia/counter/service/BookingService.java
+++ src/main/java/com/ss/training/utopia/counter/service/BookingService.java
@@ -7,23 +7,29 @@ import com.ss.training.utopia.counter.dao.AirportDao;
 import com.ss.training.utopia.counter.dao.BookingDao;
 import com.ss.training.utopia.counter.dao.FlightDao;
 import com.ss.training.utopia.counter.dao.UserDao;
+import com.ss.training.utopia.counter.entity.User;
 
 /**
  * @author Justin O'Brien
  */
 @Component
 public class BookingService {
-	
+
 	@Autowired
 	AirportDao airportDao;
-	
 	@Autowired
 	UserDao userDao;
-	
 	@Autowired
 	FlightDao flightDao;
-	
 	@Autowired
 	BookingDao bookingDao;
+
+	public Long createUser(User user) {
+		try {
+			return userDao.save(user).getUserId();
+		} catch (Throwable t) {
+			return null;
+		}
+	}
 
 }
