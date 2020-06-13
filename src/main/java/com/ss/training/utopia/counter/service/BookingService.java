bf593141b161580d63fdf5b8517a4797e0567c9e
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-13 15:01:18 -0600

    Autowire DAOs in booking service

--- src/main/java/com/ss/training/utopia/counter/service/BookingService.java
+++ src/main/java/com/ss/training/utopia/counter/service/BookingService.java
@@ -1,11 +1,29 @@
 package com.ss.training.utopia.counter.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.ss.training.utopia.counter.dao.AirportDao;
+import com.ss.training.utopia.counter.dao.BookingDao;
+import com.ss.training.utopia.counter.dao.FlightDao;
+import com.ss.training.utopia.counter.dao.UserDao;
 
 /**
  * @author Justin O'Brien
  */
 @Component
 public class BookingService {
+	
+	@Autowired
+	AirportDao airportDao;
+	
+	@Autowired
+	UserDao userDao;
+	
+	@Autowired
+	FlightDao flightDao;
+	
+	@Autowired
+	BookingDao bookingDao;
 
 }
