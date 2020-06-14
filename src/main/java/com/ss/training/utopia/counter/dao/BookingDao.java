1bbb10403a443697b591528bb154fb115ee8ea34
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-14 14:53:03 -0600

    Add repository annotation to booking DAO

--- src/main/java/com/ss/training/utopia/counter/dao/BookingDao.java
+++ src/main/java/com/ss/training/utopia/counter/dao/BookingDao.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.ss.training.utopia.counter.entity.Booking;
 import com.ss.training.utopia.counter.entity.BookingPk;
@@ -11,6 +12,7 @@ import com.ss.training.utopia.counter.entity.BookingPk;
 /**
  * @author Justin O'Brien
  */
+@Repository
 public interface BookingDao extends JpaRepository<Booking, BookingPk> {
 
 	@Query("SELECT b FROM tbl_booking b JOIN tbl_flight f WHERE b.travelerId = ?1 AND b.active = true AND f.departTime > CURRENT_TIMESTAMP")
