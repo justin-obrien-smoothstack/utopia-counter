c6bcfa37e15e3fed8f2549d4332d120a5be1fb19
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-14 11:43:41 -0600

    Write a test for booking service
    It doesn't work yet

--- src/test/java/com/ss/training/utopia/test/counter/service/BookingServiceTest.java
+++ src/test/java/com/ss/training/utopia/test/counter/service/BookingServiceTest.java
@@ -1,10 +1,36 @@
 package com.ss.training.utopia.test.counter.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ss.training.utopia.counter.dao.UserDao;
+import com.ss.training.utopia.counter.entity.User;
+import com.ss.training.utopia.counter.service.BookingService;
+
 /**
  * @author Justin O'Brien
  */
+@SpringBootTest
 public class BookingServiceTest {
 
-	
-	
+	@Mock
+	private UserDao userDao;
+
+	@InjectMocks
+	private BookingService bookingService;
+
+	@Test
+	public void createUserTest() {
+		Long mockUserId = (long) 1;
+		User mockNewUser = new User(), mockSavedUser = new User();
+		Mockito.when(mockSavedUser.getUserId()).thenReturn(mockUserId);
+		Mockito.when(userDao.save(mockNewUser)).thenReturn(mockSavedUser);
+		assertEquals(mockUserId, bookingService.createUser(mockNewUser));
+	}
+
 }
