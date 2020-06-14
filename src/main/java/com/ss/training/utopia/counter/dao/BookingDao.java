ec0542e9cb8b36484bc5e5559debe437db2e53be
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-14 13:16:02 -0600

    Correct query in method to find cancellable bookings

--- src/main/java/com/ss/training/utopia/counter/dao/BookingDao.java
+++ src/main/java/com/ss/training/utopia/counter/dao/BookingDao.java
@@ -13,7 +13,7 @@ import com.ss.training.utopia.counter.entity.BookingPk;
  */
 public interface BookingDao extends JpaRepository<Booking, BookingPk> {
 
-	@Query("SELECT b FROM tbl_booking b JOIN tbl_flight f WHERE b.travelerId = ?1 AND b.active = ?2 AND f.departTime > CURRENT_TIMESTAMP")
-	public List<Booking> findCancellable(Integer travelerId, Boolean active);
+	@Query("SELECT b FROM tbl_booking b JOIN tbl_flight f WHERE b.travelerId = ?1 AND b.active = true AND f.departTime > CURRENT_TIMESTAMP")
+	public List<Booking> findCancellable(Integer travelerId);
 
 }
