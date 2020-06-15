3af86d289f9e1ffbddcdc547653031140edad6d0
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-15 11:52:05 -0600

    Change booking service annotation from Component to Service

--- src/main/java/com/ss/training/utopia/counter/service/BookingService.java
+++ src/main/java/com/ss/training/utopia/counter/service/BookingService.java
@@ -3,7 +3,7 @@ package com.ss.training.utopia.counter.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ss.training.utopia.counter.dao.AirportDao;
@@ -18,7 +18,7 @@ import com.ss.training.utopia.counter.entity.User;
 /**
  * @author Justin O'Brien
  */
-@Component
+@Service
 public class BookingService {
 
 	@Autowired
