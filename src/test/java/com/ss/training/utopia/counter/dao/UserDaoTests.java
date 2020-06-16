deefacbc57468ccd9f5466503bca71ddbef0bd2e
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-15 20:14:48 -0600

    Remove unused imports from user DAO test class

--- src/test/java/com/ss/training/utopia/counter/dao/UserDaoTests.java
+++ src/test/java/com/ss/training/utopia/counter/dao/UserDaoTests.java
@@ -3,11 +3,9 @@ package com.ss.training.utopia.counter.dao;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.ss.training.utopia.counter.entity.User;
 
