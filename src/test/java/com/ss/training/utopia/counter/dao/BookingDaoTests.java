8153672dab3fa1983cacda7a2bb7e9c48f3af1f7
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-15 20:15:54 -0600

    Remove unused imports from booking DAO test class

--- src/test/java/com/ss/training/utopia/counter/dao/BookingDaoTests.java
+++ src/test/java/com/ss/training/utopia/counter/dao/BookingDaoTests.java
@@ -7,11 +7,9 @@ import java.time.Instant;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.ss.training.utopia.counter.entity.Booking;
 import com.ss.training.utopia.counter.entity.Flight;
