2e9029e8bf2f6517635be288bf560cc0daaa7699
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-17 16:27:10 -0600

    Write test of DAO method to find cancellably booked flights

--- src/test/java/com/ss/training/utopia/counter/dao/FlightDaoTests.java
+++ src/test/java/com/ss/training/utopia/counter/dao/FlightDaoTests.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.jupiter.api.AfterEach;
@@ -85,6 +87,33 @@ public class FlightDaoTests {
 		testEntityManager.persist(thisFlight);
 		testEntityManager.persist(otherFlight);
 		assertEquals(thisFlight, flightDao.findByFlightId(thisFlightId));
+	}
+
+	@Test
+	public void findCancellablyBookedTest() {
+		final Long HOUR = 3_600_000l;
+		Long thisTravelerId = 1l, otherTravelerId = 2l, futureFlightId = 1l, pastFlightId = 2l,
+				otherFutureFlightId = 3l, now = Instant.now().toEpochMilli();
+		Timestamp past = new Timestamp(now - HOUR), future = new Timestamp(now + HOUR);
+		Flight cancellablyBookedFlight = new Flight(1l, 2l, future, futureFlightId, null, null),
+				pastFlight = new Flight(1l, 2l, past, pastFlightId, null, null),
+				unbookedByThisTravelerFlight = new Flight(2l, 1l, future, otherFutureFlightId, null, null);
+		Booking cancellableBooking = new Booking(thisTravelerId, futureFlightId, null, true, null),
+				otherTravelerBooking = new Booking(otherTravelerId, futureFlightId, null, true, null),
+				pastFlightBooking = new Booking(thisTravelerId, pastFlightId, null, true, null),
+				inactiveBooking = new Booking(thisTravelerId, otherFutureFlightId, null, false, null);
+		List<Flight> expectedFlights = new ArrayList<Flight>(), foundFlights;
+		expectedFlights.add(cancellablyBookedFlight);
+		testEntityManager.persist(cancellablyBookedFlight);
+		testEntityManager.persist(pastFlight);
+		testEntityManager.persist(unbookedByThisTravelerFlight);
+		testEntityManager.persist(cancellableBooking);
+		testEntityManager.persist(otherTravelerBooking);
+		testEntityManager.persist(pastFlightBooking);
+		testEntityManager.persist(inactiveBooking);
+		testEntityManager.flush();
+		foundFlights = flightDao.findCancellablyBooked(thisTravelerId);
+		assertEquals(expectedFlights, foundFlights);
 	}
 
 }
