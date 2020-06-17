d329bea88c4fafdb890108302560eecdbf0a0aa2
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-17 17:59:13 -0600

    Write service method to cancel booking

--- src/main/java/com/ss/training/utopia/counter/service/CancellationService.java
+++ src/main/java/com/ss/training/utopia/counter/service/CancellationService.java
@@ -3,8 +3,12 @@ package com.ss.training.utopia.counter.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ss.training.utopia.counter.dao.BookingDao;
 import com.ss.training.utopia.counter.dao.FlightDao;
+import com.ss.training.utopia.counter.entity.Booking;
+import com.ss.training.utopia.counter.entity.BookingPk;
 import com.ss.training.utopia.counter.entity.Flight;
 
 /**
@@ -14,13 +18,39 @@ public class CancellationService {
 
 	@Autowired
 	FlightDao flightDao;
+	@Autowired
+	BookingDao bookingDao;
 
-	public List<Flight> getCancellablyBookedFlights(Long userId) {
+	public List<Flight> getCancellablyBookedFlights(Long travelerId) {
 		try {
-			return flightDao.findCancellablyBooked(userId);
+			return flightDao.findCancellablyBooked(travelerId);
 		} catch (Throwable t) {
 			return null;
 		}
+	}
+
+	@Transactional
+	public Boolean cancelBooking(Long travelerId, Long flightId) {
+		Flight flight;
+		Booking booking;
+		try {
+			flight = flightDao.findByFlightId(flightId);
+			booking = bookingDao.findById(new BookingPk(travelerId, flightId)).get();
+		} catch (Throwable t) {
+			return null;
+		}
+		if (!booking.isActive())
+			return false;
+		flight.setSeatsAvailable((short) (flight.getSeatsAvailable() + 1));
+		booking.setActive(false);
+		try {
+			flightDao.save(flight);
+			bookingDao.save(booking);
+		} catch (Throwable t) {
+			return null;
+		}
+		// issue Stripe refund
+		return true;
 	}
 
 }
