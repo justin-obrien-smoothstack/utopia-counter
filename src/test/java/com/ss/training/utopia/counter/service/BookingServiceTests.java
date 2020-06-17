e2960b99f099276d63e276915eb4eb1eec1e0bc3
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-16 20:08:52 -0600

    Write 100% test for service method to create booking

--- src/test/java/com/ss/training/utopia/counter/service/BookingServiceTests.java
+++ src/test/java/com/ss/training/utopia/counter/service/BookingServiceTests.java
@@ -1,5 +1,6 @@
 package com.ss.training.utopia.counter.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -19,9 +20,11 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.ss.training.utopia.counter.dao.AirportDao;
+import com.ss.training.utopia.counter.dao.BookingDao;
 import com.ss.training.utopia.counter.dao.FlightDao;
 import com.ss.training.utopia.counter.dao.UserDao;
 import com.ss.training.utopia.counter.entity.Airport;
+import com.ss.training.utopia.counter.entity.Booking;
 import com.ss.training.utopia.counter.entity.Flight;
 import com.ss.training.utopia.counter.entity.User;
 
@@ -37,6 +40,8 @@ public class BookingServiceTests {
 	private AirportDao airportDao;
 	@Mock
 	private FlightDao flightDao;
+	@Mock
+	private BookingDao bookingDao;
 	@InjectMocks
 	private BookingService bookingService;
 
@@ -99,6 +104,26 @@ public class BookingServiceTests {
 		assertTrue(Arrays.equals(flightArray, bookingService.getBookableFlights(null, null, null)));
 		Mockito.when(flightDao.findBookable(null, null, null)).thenThrow(new RuntimeException());
 		assertNull(bookingService.getBookableFlights(null, null, null));
+	}
+
+	@Test
+	public void bookFlightTest() {
+		Booking booking = new Booking();
+		Flight flight = new Flight(null, null, null, null, (short) 1, null);
+		Mockito.when(flightDao.findByFlightId(null)).thenReturn(flight);
+		assertTrue(bookingService.bookFlight(booking));
+		assertEquals((short) 0, flight.getSeatsAvailable());
+		assertFalse(bookingService.bookFlight(booking));
+		flight.setSeatsAvailable((short) 10);
+		Mockito.when(flightDao.findByFlightId(null)).thenThrow(new RuntimeException());
+		assertNull(bookingService.bookFlight(booking));
+		Mockito.reset(flightDao);
+		Mockito.when(flightDao.findByFlightId(null)).thenReturn(flight);
+		Mockito.when(bookingDao.save(booking)).thenThrow(new RuntimeException());
+		assertNull(bookingService.bookFlight(booking));
+		Mockito.reset(bookingDao);
+		Mockito.when(flightDao.save(flight)).thenThrow(new RuntimeException());
+		assertNull(bookingService.bookFlight(booking));
 	}
 
 }
