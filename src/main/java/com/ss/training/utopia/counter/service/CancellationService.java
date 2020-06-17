b47b3493495a35e6bbc9d412c7fdb4c08e32972e
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-17 16:54:41 -0600

    Write service function to get cancellably booked flights

--- src/main/java/com/ss/training/utopia/counter/service/CancellationService.java
+++ src/main/java/com/ss/training/utopia/counter/service/CancellationService.java
@@ -1,8 +1,26 @@
 package com.ss.training.utopia.counter.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ss.training.utopia.counter.dao.FlightDao;
+import com.ss.training.utopia.counter.entity.Flight;
+
 /**
  * @author Justin O'Brien
  */
 public class CancellationService {
+
+	@Autowired
+	FlightDao flightDao;
+
+	public List<Flight> getCancellablyBookedFlights(Long userId) {
+		try {
+			return flightDao.findCancellablyBooked(userId);
+		} catch (Throwable t) {
+			return null;
+		}
+	}
 
 }
