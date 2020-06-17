dccb37ecac27195d16287c01161bffb2e7b46f4f
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-17 16:26:20 -0600

    Remove DAO method to find cancellable bookings

--- src/main/java/com/ss/training/utopia/counter/dao/BookingDao.java
+++ src/main/java/com/ss/training/utopia/counter/dao/BookingDao.java
@@ -1,9 +1,6 @@
 package com.ss.training.utopia.counter.dao;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ss.training.utopia.counter.entity.Booking;
@@ -14,9 +11,5 @@ import com.ss.training.utopia.counter.entity.BookingPk;
  */
 @Repository
 public interface BookingDao extends JpaRepository<Booking, BookingPk> {
-
-	@Query("SELECT b FROM Booking b WHERE b.travelerId = ?1 AND b.active = true AND b.flightId NOT IN "
-			+ "(SELECT f.flightId FROM Flight f WHERE f.departTime <= CURRENT_TIMESTAMP)")
-	public List<Booking> findCancellable(Long travelerId);
 
 }
