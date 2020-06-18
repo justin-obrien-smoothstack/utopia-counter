026a070c0c4c2b128a5b9422f810748f4a384dc4
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-18 16:15:22 -0600

    Add Service annotation to cancellation service

--- src/main/java/com/ss/training/utopia/counter/service/CancellationService.java
+++ src/main/java/com/ss/training/utopia/counter/service/CancellationService.java
@@ -3,6 +3,7 @@ package com.ss.training.utopia.counter.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ss.training.utopia.counter.dao.BookingDao;
@@ -14,6 +15,7 @@ import com.ss.training.utopia.counter.entity.Flight;
 /**
  * @author Justin O'Brien
  */
+@Service
 public class CancellationService {
 
 	@Autowired
