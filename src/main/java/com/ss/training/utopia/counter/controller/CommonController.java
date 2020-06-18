7a87eb9766dafdceccb710fcc342581d24c5fb12
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-18 15:51:54 -0600

    Annotate common controller class

--- src/main/java/com/ss/training/utopia/counter/controller/CommonController.java
+++ src/main/java/com/ss/training/utopia/counter/controller/CommonController.java
@@ -1,8 +1,13 @@
 package com.ss.training.utopia.counter.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Justin O'Brien
  */
+@RestController
+@RequestMapping(path="/counter")
 public class CommonController {
 
 }
