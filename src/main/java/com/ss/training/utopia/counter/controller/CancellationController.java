a435745c5865aeb07ab1f8dbbd3d18084881992c
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-18 21:05:53 -0600

    Write controller method to cancel booking

--- src/main/java/com/ss/training/utopia/counter/controller/CancellationController.java
+++ src/main/java/com/ss/training/utopia/counter/controller/CancellationController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +34,20 @@ public class CancellationController {
 		if (flights != null && flights.length == 0)
 			status = HttpStatus.NO_CONTENT;
 		return new ResponseEntity<Flight[]>(flights, status);
+	}
+
+	@PutMapping(path = "/bookings/traveler/{travelerId}/flight/{flightId}")
+	public ResponseEntity<Object> cancelBooking(@PathVariable Long travelerId, @PathVariable long flightId) {
+		Boolean goodRequest = null;
+		HttpStatus status = HttpStatus.NO_CONTENT;
+		try {
+			goodRequest = service.cancelBooking(travelerId, flightId);
+		} catch (Throwable t) {
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		if (goodRequest != null && !goodRequest)
+			status = HttpStatus.BAD_REQUEST;
+		return new ResponseEntity<Object>(null, status);
 	}
 
 }
