0fc4068c67e4296ddcb96f3c8cb78201ff97e14e
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-18 20:45:53 -0600

    Test CancellationController.getCancellablyBookedFlights

--- src/test/java/com/ss/training/utopia/counter/controller/CancellationControllerTests.java
+++ src/test/java/com/ss/training/utopia/counter/controller/CancellationControllerTests.java
@@ -1,11 +1,21 @@
 package com.ss.training.utopia.counter.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ss.training.utopia.counter.entity.Flight;
 import com.ss.training.utopia.counter.service.CancellationService;
 
 /**
@@ -20,5 +30,20 @@ public class CancellationControllerTests {
 	private ObjectMapper mapper;
 	@MockBean
 	private CancellationService service;
-	
+
+	@Test
+	public void getCancellablyBookedFlightsTest() throws Exception {
+		final Long HOUR = 3_600_000l;
+		Long travelerId = 6l, now = Instant.now().toEpochMilli();
+		Timestamp future = new Timestamp(now + HOUR), futureTwo = new Timestamp(now + 2 * HOUR);
+		Flight[] flights = { new Flight(4l, 6l, future, 2l, (short) 3, 150f),
+				new Flight(6l, 4l, futureTwo, 3l, (short) 2, 151f) }, noFlights = {};
+		String uri = "/counter/flights/cancellable/" + travelerId, expectedContent = mapper.writeValueAsString(flights);
+		Mockito.when(service.getCancellablyBookedFlights(travelerId)).thenReturn(flights, noFlights);
+		mvc.perform(get(uri)).andExpect(status().isOk()).andExpect(content().string(expectedContent));
+		mvc.perform(get(uri)).andExpect(status().isNoContent()).andExpect(content().string("[]"));
+		Mockito.when(service.getCancellablyBookedFlights(travelerId)).thenThrow(new RuntimeException());
+		mvc.perform(get(uri)).andExpect(status().isInternalServerError()).andExpect(content().string(""));
+	}
+
 }
