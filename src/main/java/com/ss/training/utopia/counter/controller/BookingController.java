62aa5912fcb1c5a2aeb90b21ed403dde4fc7bf99
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-19 16:52:20 -0600

    Write controller method to check username availability

--- src/main/java/com/ss/training/utopia/counter/controller/BookingController.java
+++ src/main/java/com/ss/training/utopia/counter/controller/BookingController.java
@@ -3,6 +3,8 @@ package com.ss.training.utopia.counter.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +33,20 @@ public class BookingController {
 			status = HttpStatus.INTERNAL_SERVER_ERROR;
 		}
 		return new ResponseEntity<User>(user, status);
+	}
+
+	@GetMapping("/user/{username}")
+	public ResponseEntity<Object> usernameAvailable(@PathVariable String username) {
+		Boolean available = null;
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		try {
+			available = service.usernameAvailable(username);
+		} catch (Throwable t) {
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		if (available != null && !available)
+			status = HttpStatus.NO_CONTENT;
+		return new ResponseEntity<Object>(null, status);
 	}
 
 }
