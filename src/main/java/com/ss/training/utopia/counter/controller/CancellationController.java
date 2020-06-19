5e5ffaa5e821ea82ecb7f57ce70ac268aa67e2db
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-18 20:10:16 -0600

    Start cancellation controller & test classes

---
+++ src/main/java/com/ss/training/utopia/counter/controller/CancellationController.java
@@ -0,0 +1,13 @@
+package com.ss.training.utopia.counter.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Justin O'Brien
+ */
+@RestController
+@RequestMapping("/counter")
+public class CancellationController {
+
+}
---
+++ src/test/java/com/ss/training/utopia/counter/controller/CancellationControllerTests.java
@@ -0,0 +1,23 @@
+package com.ss.training.utopia.counter.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * @author Justin O'Brien
+ */
+@WebMvcTest(CancellationController.class)
+public class CancellationControllerTests {
+
+	@Autowired
+	private MockMvc mvc;
+	@Autowired
+	private ObjectMapper mapper;
+	@MockBean
+	private CancellationService service;
+	
+}
