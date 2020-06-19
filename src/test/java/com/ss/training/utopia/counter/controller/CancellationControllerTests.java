6278ae4dc3df29ee172534c03f6f8df2ed6b4d30
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-18 21:06:00 -0600

    Test controller method to cancel booking

--- src/test/java/com/ss/training/utopia/counter/controller/CancellationControllerTests.java
+++ src/test/java/com/ss/training/utopia/counter/controller/CancellationControllerTests.java
@@ -1,6 +1,8 @@
 package com.ss.training.utopia.counter.controller;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -39,11 +41,22 @@ public class CancellationControllerTests {
 		Flight[] flights = { new Flight(4l, 6l, future, 2l, (short) 3, 150f),
 				new Flight(6l, 4l, futureTwo, 3l, (short) 2, 151f) }, noFlights = {};
 		String uri = "/counter/flights/cancellable/" + travelerId, expectedContent = mapper.writeValueAsString(flights);
-		Mockito.when(service.getCancellablyBookedFlights(travelerId)).thenReturn(flights, noFlights);
+		when(service.getCancellablyBookedFlights(travelerId)).thenReturn(flights, noFlights);
 		mvc.perform(get(uri)).andExpect(status().isOk()).andExpect(content().string(expectedContent));
 		mvc.perform(get(uri)).andExpect(status().isNoContent()).andExpect(content().string("[]"));
-		Mockito.when(service.getCancellablyBookedFlights(travelerId)).thenThrow(new RuntimeException());
+		when(service.getCancellablyBookedFlights(travelerId)).thenThrow(new RuntimeException());
 		mvc.perform(get(uri)).andExpect(status().isInternalServerError()).andExpect(content().string(""));
+	}
+
+	@Test
+	public void cancelBookingTest() throws Exception {
+		Long travelerId = 4l, flightId = 6l;
+		String uri = "/counter/bookings/traveler/" + travelerId + "/flight/" + flightId;
+		when(service.cancelBooking(travelerId, flightId)).thenReturn(true, false);
+		mvc.perform(put(uri)).andExpect(status().isNoContent()).andExpect(content().string(""));
+		mvc.perform(put(uri)).andExpect(status().isBadRequest()).andExpect(content().string(""));
+		when(service.cancelBooking(travelerId, flightId)).thenThrow(new RuntimeException());
+		mvc.perform(put(uri)).andExpect(status().isInternalServerError()).andExpect(content().string(""));
 	}
 
 }
