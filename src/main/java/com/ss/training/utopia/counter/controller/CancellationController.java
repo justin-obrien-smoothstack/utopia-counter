433e9e19439a2cba077a95d399ba404e78b85e03
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-18 20:45:46 -0600

    Write CancellationController.getCancellablyBookedFlights

--- src/main/java/com/ss/training/utopia/counter/controller/CancellationController.java
+++ src/main/java/com/ss/training/utopia/counter/controller/CancellationController.java
@@ -1,7 +1,15 @@
 package com.ss.training.utopia.counter.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ss.training.utopia.counter.entity.Flight;
+import com.ss.training.utopia.counter.service.CancellationService;
 
 /**
  * @author Justin O'Brien
@@ -9,5 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/counter")
 public class CancellationController {
+
+	@Autowired
+	CancellationService service;
+
+	@GetMapping(path = "/flights/cancellable/{travelerId}")
+	public ResponseEntity<Flight[]> getCancellablyBookedFlights(@PathVariable Long travelerId) {
+		Flight[] flights = null;
+		HttpStatus status = HttpStatus.OK;
+		try {
+			flights = service.getCancellablyBookedFlights(travelerId);
+		} catch (Throwable t) {
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		if (flights != null && flights.length == 0)
+			status = HttpStatus.NO_CONTENT;
+		return new ResponseEntity<Flight[]>(flights, status);
+	}
 
 }
