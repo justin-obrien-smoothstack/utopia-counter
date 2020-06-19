8d7a4d3a6e21c503049fac4f44fbd86706ec198a
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-19 17:26:53 -0600

    Test controller method that gets bookable flights

--- src/test/java/com/ss/training/utopia/counter/controller/BookingControllerTests.java
+++ src/test/java/com/ss/training/utopia/counter/controller/BookingControllerTests.java
@@ -6,6 +6,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,6 +18,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ss.training.utopia.counter.entity.Flight;
 import com.ss.training.utopia.counter.entity.User;
 import com.ss.training.utopia.counter.service.BookingService;
 
@@ -53,6 +57,22 @@ public class BookingControllerTests {
 		mvc.perform(get(uri)).andExpect(status().isNotFound()).andExpect(content().string(""));
 		mvc.perform(get(uri)).andExpect(status().isNoContent()).andExpect(content().string(""));
 		when(service.usernameAvailable(username)).thenThrow(new RuntimeException());
+		mvc.perform(get(uri)).andExpect(status().isInternalServerError()).andExpect(content().string(""));
+	}
+
+	@Test
+	public void getBookableFlightsTest() throws Exception {
+		final Long HOUR = 3_600_000l;
+		Long departId = 4l, arriveId = 2l, travelerId = 6l, now = Instant.now().toEpochMilli();
+		Timestamp futureOne = new Timestamp(now + HOUR), futureTwo = new Timestamp(now + 2 * HOUR);
+		Flight[] flights = { new Flight(departId, arriveId, futureOne, 3l, (short) 8, 150f),
+				new Flight(departId, arriveId, futureTwo, 7l, (short) 5, 151f) };
+		String uri = "/counter/flights/bookable/departure/" + departId + "/arrival/" + arriveId + "/traveler/"
+				+ travelerId, expectedContent = mapper.writeValueAsString(flights);
+		when(service.getBookableFlights(departId, arriveId, travelerId)).thenReturn(flights, new Flight[0]);
+		mvc.perform(get(uri)).andExpect(status().isOk()).andExpect(content().string(expectedContent));
+		mvc.perform(get(uri)).andExpect(status().isNoContent()).andExpect(content().string("[]"));
+		when(service.getBookableFlights(departId, arriveId, travelerId)).thenThrow(new RuntimeException());
 		mvc.perform(get(uri)).andExpect(status().isInternalServerError()).andExpect(content().string(""));
 	}
 
