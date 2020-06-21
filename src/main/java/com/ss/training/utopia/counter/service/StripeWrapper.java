03a112850ea6eaa69e18c4d8db06ad1510a40d2e
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-20 20:55:41 -0600

    Add Stripe key to methods in Stripe wrapper

--- src/main/java/com/ss/training/utopia/counter/service/StripeWrapper.java
+++ src/main/java/com/ss/training/utopia/counter/service/StripeWrapper.java
@@ -2,6 +2,7 @@ package com.ss.training.utopia.counter.service;
 
 import org.springframework.stereotype.Component;
 
+import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Charge;
 import com.stripe.model.Refund;
@@ -15,10 +16,12 @@ import com.stripe.param.RefundCreateParams;
 public class StripeWrapper {
 
 	public String createChargeGetId(ChargeCreateParams params) throws StripeException {
+		Stripe.apiKey = "sk_test_51GwErbJwa8c7tq3Odc3WXzypPn0OPpPAd6O5gjvRBBEb15K77CX8D2XSGyyXYgbpNJ0tW52TNRY8ox0o8iKgTkqj00v7B6meHs";
 		return Charge.create(params).getId();
 	}
-	
-	public Refund createRefund(RefundCreateParams params)throws StripeException{
+
+	public Refund createRefund(RefundCreateParams params) throws StripeException {
+		Stripe.apiKey = "sk_test_51GwErbJwa8c7tq3Odc3WXzypPn0OPpPAd6O5gjvRBBEb15K77CX8D2XSGyyXYgbpNJ0tW52TNRY8ox0o8iKgTkqj00v7B6meHs";
 		return Refund.create(params);
 	}
 
