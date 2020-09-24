fa60a418a5aa4c9c8ddd0f0912584935ebd343ca
Justin O'Brien <justin.obrien@smoothstack.com>
2020-09-24 16:43:02 -0600

    Add refund method to Stripe DAO

--- src/main/java/com/ss/training/utopia/counter/dao/StripeDao.java
+++ src/main/java/com/ss/training/utopia/counter/dao/StripeDao.java
@@ -1,15 +1,25 @@
 package com.ss.training.utopia.counter.dao;
 
 import org.springframework.stereotype.Component;
-import com.stripe.Stripe;
+
 import com.ss.training.utopia.counter.Secrets;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Refund;
+import com.stripe.param.RefundCreateParams;
 
 /**
  * @author Justin O'Brien
  */
 @Component
 public class StripeDao {
+
 	static {
 		Stripe.apiKey = Secrets.stripeKey;
 	}
+
+	public void refund(String stripeId) throws StripeException {
+		Refund.create(RefundCreateParams.builder().setCharge(stripeId).build());
+	}
+
 }
