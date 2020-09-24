419a82a587cf813bfa7f61b2eaf4186474948b60
Justin O'Brien <justin.obrien@smoothstack.com>
2020-09-24 17:19:29 -0600

    Add charge method to Stripe DAO

--- src/main/java/com/ss/training/utopia/counter/dao/StripeDao.java
+++ src/main/java/com/ss/training/utopia/counter/dao/StripeDao.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Component;
 import com.ss.training.utopia.counter.Secrets;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
 import com.stripe.model.Refund;
+import com.stripe.param.ChargeCreateParams;
 import com.stripe.param.RefundCreateParams;
 
 /**
@@ -16,6 +18,11 @@ public class StripeDao {
 
 	static {
 		Stripe.apiKey = Secrets.stripeKey;
+	}
+
+	public String charge(String token, long price) throws StripeException {
+		return Charge.create(ChargeCreateParams.builder().setAmount(price).setCurrency("usd").setSource(token).build())
+				.getId();
 	}
 
 	public void refund(String chargeId) throws StripeException {
